b7874a982c51a36772a33c9e9c88a2400ccb849d
HackUnit <j.larose26@gmail.com>
2022-09-11 22:50:42 -0500

    Added majorAirports.json, modified MappingGeoJSON files and added to .gitignore

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -3,7 +3,79 @@
 console.log("working");
 
 
-// For Module work
+// Create the map object with center at the San Francisco airport. 
+
+let map = L.map("mapid", {
+    
+    center: [
+
+        37.5, -122.5
+    
+    ], 
+
+    zoom: 10
+    
+});
+
+// Add GeoJSON data.
+
+let sanFranAirport = {
+
+    "type": "FeatureCollection",
+
+    "features": [
+        
+    {
+        "type": "Feature",
+
+        "properties": {
+            "id": "3469",
+            "name": "San Francisco International Airport",
+            "city": "San Francisco",
+            "country": "United States",
+            "faa": "SFO",
+            "icao": "KSFO",
+            "alt": "13",
+            "tz-offset": "-8",
+            "dst": "A",
+            "tz": "America/Los_Angeles"
+        },
+
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-122.375, 37.61899948120117]
+        }
+    }]
+};
+
+// Grabbing our GeoJSON data.
+
+L.geoJson(sanFranAirport, {
+
+    // onEachFeature function
+    onEachFeature: function(feature, layer) {
+
+        console.log(layer);
+        layer.bindPopup("<h2>" + "Airport code: " + feature.properties.faa + "</h2>" + "<hr>" + "<h3>" + "Airport name: " + feature.properties.name + "</h3>", {maxWidth: 500});
+    }
+
+    //pointToLayer function
+    // We turn each feature into a marker on the map.
+    //pointToLayer: function(feature, latlng) {
+    //
+    //    console.log(feature);
+    //
+    //    return L.marker(latlng)
+    //    
+    //    .bindPopup("<h2>" + feature.properties.name + "</h2>" + "<hr>" + "<h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+    //}
+
+
+}).addTo(map);
+
+
+
+// For Mapping_Lines Module work
 
 // Create the map object with a center and zoom level.
 // This bit of code is nicknamed "setView() method" because the module is dumb and can't decide what it wants and the actual setView() sucks.
@@ -41,42 +113,26 @@ console.log("working");
 //    color: "yellow"
 //}).addTo(map);
 
-
-
-// For Skill Drill Mapping lines code
-
-let map = L.map("mapid", {
-    
-    center: [
-        // Map centered on continental America
-        40.7, -94.5
-    
-    ], 
-    // Map zoomed to show lines
-    zoom: 4
-    
-});
 // Mapping lines code
 // Coordinates for each point to be used in the line.
 // SFO to JFK with stops at AUS, HOU and YYZ airports as points
-let line = [
-
-    [37.6213, -122.3790],
-    [30.1975, -97.6664],
-    [29.6459, -95.2769],
-    [43.6777, -79.6248],
-    [40.6413, -73.7781]
-];
-
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-
-    color: "blue",
-    dashArray: "6.5",
-    weight: 4,
-    opacity: 0.5
-}).addTo(map);
-
+//let line = [
+//
+//    [37.6213, -122.3790],
+//    [30.1975, -97.6664],
+//    [29.6459, -95.2769],
+//    [43.6777, -79.6248],
+//    [40.6413, -73.7781]
+//];
+//
+//// Create a polyline using the line coordinates and make the line yellow.
+//L.polyline(line, {
+//
+//    color: "blue",
+//    dashArray: "6.5",
+//    weight: 4,
+//    opacity: 0.5
+//}).addTo(map);
 
 // Multiple markers code
 // Get data from cities.js
@@ -98,8 +154,6 @@ L.polyline(line, {
 //    .addTo(map);
 //});
 
-
-
 // Single marker code
 // Add a marker to the map for Los Angeles, California.
 
@@ -112,26 +166,19 @@ L.polyline(line, {
 
 // We create the tile layer that will be the background of our map.
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
-
-    //id: 'mapbox/streets-v11',
-
-    // Other map styles that can be used instead:
-    // styles/mapbox/streets-v11
-    // styles/mapbox/outdoors-v11
-    // styles/mapbox/light-v10
-    // styles/mapbox/dark-v10
-    // styles/mapbox/satellite-v9
-    // styles/mapbox/satellite-streets-v11
-
-    //tileSize: 512,
-
-    //zoomOffset: -1
 });
+
+//    // Other map styles that can be used instead:
+//    // styles/mapbox/streets-v11
+//    // styles/mapbox/outdoors-v11
+//    // styles/mapbox/light-v10
+//    // styles/mapbox/dark-v10
+//    // styles/mapbox/satellite-v9
+//    // styles/mapbox/satellite-streets-v11
 
 // Then we add our 'graymap' tile layer to the map.
 
