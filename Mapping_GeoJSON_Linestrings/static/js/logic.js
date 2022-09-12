97a2a05a104899165f7816dcbc5d1f6d47bdd303
HackUnit <j.larose26@gmail.com>
2022-09-12 07:10:39 -0500

    Modified file to show all non stop flights out of Toronto on map

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -2,8 +2,8 @@
 console.log("working");
 
 
-// We create the street view tile layer that will be the default background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the light view tile layer that will be the default background of our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -18,43 +18,59 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    "Day Navigation": light,
+    "Night Navigation": dark
 };
 
-// Create the map object with center, zoom and default layer on center of Earth
+// Create the map object with center, zoom and default layer on Toronto
 let map = L.map("mapid", {
-    center: [30, 30], 
+    center: [44.0, -80.0], 
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/HackUnit/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routs GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/HackUnit/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
  
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJson(data, {
-    
+        style: myStyle,        
         onEachFeature: function(feature, layer) {
-            layer.bindPopup("<h3>Airport code: " + feature.properties.faa + "</h3> <hr> <h3>Airport name: " + feature.properties.name + "</h3>", {maxWidth: 500}); 
-        }}).addTo(map);
+            layer.bindPopup(
+                "<h3> Airline: " 
+                + feature.properties.airline 
+                + "</h3><hr><h3> Destination: "
+                + feature.properties.dst 
+                + "</h3>", 
+                {maxWidth: 500}
+            ); 
+        }
+    })
+.addTo(map);
 });
 
-// Possible solution to adding markers and popups with correct data on them.
+// Code to add markers and popups with correct data on them.
 // Make sure to take off the .addTo(map) and last ) on the created GeoJSON layer with retrieved data, and fix formatting in the popup.
 //
 //, {
-//onEachFeature: function(feature, layer) {
-//    console.log(layer);
-//layer.bindPopup("<h3>Airport code: " + feature.properties.faa + "</h3> <hr> <h3>Airport name: " + feature.properties.name + "</h3>")
-//}).addTo(map);
+//    
+//    onEachFeature: function(feature, layer) {
+//        layer.bindPopup("<h3>Airport code: " + feature.properties.faa + "</h3> <hr> <h3>Airport name: " + feature.properties.name + "</h3>", {maxWidth: 500}); 
+//    }}).addTo(map);
+
 
 //    // Other map styles that can be used:
 //    // styles/mapbox/streets-v11
